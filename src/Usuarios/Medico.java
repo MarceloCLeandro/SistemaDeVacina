19694150bf3388a61ceed83c77dc2707928897f6
Guilherme Portella <guilhermeportella2@gmail.com>
2021-03-24 23:28:25 -0300

    first commit

---
+++ src/Usuarios/Medico.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Usuarios;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class Medico {
+    private int CRM;
+    private String especialidade;
+    private boolean residente;//yes or no
+    
+    
+}
